19c2d15c23d1cb4b6ed7c62ed5c1fd51ff3d9add
Alhassan-Hub <alhassanbah790@gmail.com>
2026-03-14 16:08:46 -1100

    Hide Firebase keys

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,18 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
 
-// We are hardcoding the keys so Next.js can't ignore them!
 const firebaseConfig = {
-  apiKey: "AIzaSyCNs843-EXQEvqJQfhZVTQ2qRnVD4_1n3g",
-  authDomain: "ayeshas-signature.firebaseapp.com",
-  projectId: "ayeshas-signature",
-  storageBucket: "ayeshas-signature.appspot.com",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Export the Database and Storage tools
 export const db = getFirestore(app);
-export const storage = getStorage(app);
